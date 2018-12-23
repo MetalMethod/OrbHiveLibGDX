e086b87b4e7badae9feeff5d8617916543d2ae98
metalMethod <igorbusquets@gmail.com>
2018-12-23 04:50:03 +0000

    fixed splash screen size render on desktop

--- core/src/com/metalmethodd/orbhive/scenes/SplashScreen.java
+++ core/src/com/metalmethodd/orbhive/scenes/SplashScreen.java
@@ -38,8 +38,8 @@ public class SplashScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
 
-        batch.draw(backgroundImage, 0, 0, SCREEN_WIDTH
-                , SCREEN_HEIGHT);
+        batch.draw(backgroundImage, 0, 0, Gdx.graphics.getWidth()
+                , Gdx.graphics.getHeight());
         batch.end();
 
         if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
