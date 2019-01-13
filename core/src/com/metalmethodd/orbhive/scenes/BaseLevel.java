3d4d1ef28f4e719d97858019789f3bda2ca4e4db
metalMethod <igorbusquets@gmail.com>
2019-01-13 01:27:32 +0000

    Refactor of BaseLevel and LevelOne

--- core/src/com/metalmethodd/orbhive/scenes/BaseLevel.java
+++ core/src/com/metalmethodd/orbhive/scenes/BaseLevel.java
@@ -6,13 +6,12 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.metalmethodd.orbhive.GameInputHandler;
-import com.metalmethodd.orbhive.OrbHiveGame;
-import com.metalmethodd.orbhive.Player;
-import com.metalmethodd.orbhive.TextureHandler;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.metalmethodd.orbhive.*;
+import com.metalmethodd.orbhive.enemys.Wasp;
 
-import static com.metalmethodd.orbhive.Constants.SCREEN_HEIGHT;
-import static com.metalmethodd.orbhive.Constants.SCREEN_WIDTH;
+import static com.metalmethodd.orbhive.Constants.*;
 
 
 public class BaseLevel implements Screen {
@@ -26,12 +25,22 @@ public class BaseLevel implements Screen {
 
     protected float runTime;
 
-    public BaseLevel(OrbHiveGame game){
+    protected Player player;
+    protected Array<Wasp> enemies;
+    protected Array<Bullet> bullets;
+
+
+    public BaseLevel(OrbHiveGame game) {
         this.game = game;
         this.batch = game.batch;
         camera = new OrthographicCamera();
         camera.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT);
 
+        player = new Player(new Vector2(INITIAL_PLAYER_X, INITIAL_PLAYER_Y));
+        enemies = new Array<Wasp>();
+        bullets = new Array<Bullet>();
+
+        gameInputHandler = new GameInputHandler(player);
         textureHandler = new TextureHandler();
     }
 
@@ -79,15 +88,104 @@ public class BaseLevel implements Screen {
         textureHandler.dispose();
     }
 
-    public void checkExitGame(){
-        if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
+    protected void updateLevelBasicLogic(float delta) {
+        runTime += delta;
+        checkExitGame();
+        player.update(delta);
+
+        gameOverCondition();
+
+        updateEnemies();
+        updateBullets();
+
+        checkCollisionBulletsEnemies(bullets, enemies);
+    }
+
+    public void checkExitGame() {
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
             Gdx.app.exit();
+        }
+    }
+
+    protected void gameOverCondition() {
+        if (player.getState() == Player.EntityState.DEAD) {
+            game.setScreen(new GameOverScreen(game));
         }
     }
 
     protected void drawBackgroundColor() {
         Gdx.gl.glClearColor(0.15f, 0.15f, 0.15f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    protected void drawPlayer() {
+        textureHandler.drawPlayer(player, runTime);
+        //textureHandler.drawPlayerBoundingRect(player);
+    }
+
+    /**
+     * All levels must overide drawBackground()
+     */
+    protected void drawBackground() {
+        drawBackgroundColor();
+    }
+
+    protected void drawEnemies() {
+        for (Wasp wasp : enemies) {
+            textureHandler.drawWasp(runTime, wasp);
+            //textureHandler.drawEnemyBoundingRect(wasp);
+        }
+    }
+
+    protected void drawBullets() {
+        for (Bullet bullet : bullets) {
+            textureHandler.drawPlayerBulletRect(bullet);
+        }
+    }
+
+    protected void updateEnemies() {
+        for (Wasp wasp : enemies) {
+            wasp.update();
+
+            if (wasp.getBoundingRectangle().x < 0) {
+                enemies.removeValue(wasp, false);
+                continue;
+            }
+
+            if (wasp.getBoundingRectangle().overlaps(player.getBoundingRectangle())) {
+                enemies.removeValue(wasp, false);
+                player.takeHit(Constants.PLAYER_HIT_ACCELERATION);
+                enemies.add(EnemyFactory.createWasp());
+            }
+        }
+    }
+
+    protected void updateBullets() {
+        bullets = player.getBullets();
+
+        for (Bullet bullet : bullets) {
+            bullet.update();
+
+            if (bullet.getPosition().x >= Constants.SCREEN_WIDTH) {
+                bullets.removeValue(bullet, false);
+            }
+        }
+    }
+
+    protected void checkCollisionBulletsEnemies(Array<Bullet> bullets, Array<Wasp> enemies) {
+        for (Wasp wasp : enemies) {
+            for (Bullet bullet : bullets) {
+                if (wasp.getBoundingRectangle().overlaps(bullet.getBoundingRectangle())) {
+                    killWasp(wasp);
+                    bullets.removeValue(bullet, false);
+                }
+            }
+        }
+    }
+
+    protected void killWasp(Wasp wasp) {
+        System.out.println("Enemy HIT");
+        enemies.removeValue(wasp, false);
     }
 
 }
--- core/src/com/metalmethodd/orbhive/scenes/LevelOne.java
+++ core/src/com/metalmethodd/orbhive/scenes/LevelOne.java
@@ -1,130 +1,47 @@
 package com.metalmethodd.orbhive.scenes;
 
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Array;
 import com.metalmethodd.orbhive.*;
-import com.metalmethodd.orbhive.enemys.Wasp;
-
-import static com.metalmethodd.orbhive.Constants.INITIAL_PLAYER_X;
-import static com.metalmethodd.orbhive.Constants.INITIAL_PLAYER_Y;
 
 public class LevelOne extends BaseLevel {
-
-    private Player player;
-    private Wasp wasp;
-    private Array<Wasp> enemies;
-    private Array<Bullet> bullets;
 
     float previousSpawn;
 
     public LevelOne(OrbHiveGame game) {
         super(game);
-
-        player = new Player(new Vector2(INITIAL_PLAYER_X, INITIAL_PLAYER_Y));
-        gameInputHandler = new GameInputHandler(player);
-        enemies = new Array<Wasp>();
-        wasp = EnemyFactory.createWasp();
-        bullets = new Array<Bullet>();
-
         previousSpawn = runTime;
     }
 
-
+    /**
+     * All levels must call update(delta) on render.
+     *
+     * @param delta
+     */
     public void render(float delta) {
-        updateLevel(delta);
-
+        update(delta);
         drawBackground();
         drawPlayer();
         drawEnemies();
         drawBullets();
     }
 
-    private void updateLevel(float delta) {
-        runTime += delta;
-        checkExitGame();
-        player.update(delta);
-
-        gameOverCondition();
-
+    /**
+     * It is required for all levels, to call updateLevelBasicLogic()
+     * so all basic mechanics of each level is handled by the BaseLevel class
+     *
+     * @param delta
+     */
+    private void update(float delta) {
+        updateLevelBasicLogic(delta);
         spawnEnemies();
-        updateEnemies();
-        updateBullets();
-
-        checkCollisionBulletsEnemies(bullets, enemies);
     }
 
-    private void drawPlayer() {
-        textureHandler.drawPlayer(player, runTime);
-        //textureHandler.drawPlayerBoundingRect(player);
-    }
-
-    private void drawBackground() {
+    /**
+     * All levels must overide drawBackground()
+     */
+    @Override
+    protected void drawBackground() {
         drawBackgroundColor();
         textureHandler.drawBgLevelOne();
-    }
-
-    private void drawEnemies() {
-        for(Wasp wasp : enemies) {
-            textureHandler.drawWasp(runTime, wasp);
-            //textureHandler.drawEnemyBoundingRect(wasp);
-        }
-    }
-
-    private void drawBullets() {
-        for(Bullet bullet : bullets){
-            textureHandler.drawPlayerBulletRect(bullet);
-        }
-    }
-
-    private void gameOverCondition() {
-        if (player.getState() == Player.EntityState.DEAD) {
-            game.setScreen(new GameOverScreen(game));
-        }
-    }
-
-    private void updateEnemies() {
-        for (Wasp wasp : enemies) {
-            wasp.update();
-
-            if (wasp.getBoundingRectangle().x < 0) {
-                enemies.removeValue(wasp, false);
-                continue;
-            }
-
-            if (wasp.getBoundingRectangle().overlaps(player.getBoundingRectangle())) {
-                enemies.removeValue(wasp, false);
-                player.takeHit(Constants.PLAYER_HIT_ACCELERATION);
-                enemies.add(EnemyFactory.createWasp());
-            }
-        }
-    }
-
-    private void updateBullets() {
-        bullets = player.getBullets();
-
-        for (Bullet bullet : bullets) {
-            bullet.update();
-
-            if (bullet.getPosition().x >= Constants.SCREEN_WIDTH) {
-                bullets.removeValue(bullet, false);
-            }
-        }
-    }
-
-    private void checkCollisionBulletsEnemies(Array<Bullet> bullets, Array<Wasp> enemies) {
-        for (Wasp wasp : enemies) {
-            for (Bullet bullet : bullets) {
-                if (wasp.getBoundingRectangle().overlaps(bullet.getBoundingRectangle())) {
-                    killWasp(wasp);
-                    bullets.removeValue(bullet, false);
-                }
-            }
-        }
-    }
-
-    private void killWasp(Wasp wasp) {
-        System.out.println("Enemy HIT");
-        enemies.removeValue(wasp, false);
     }
 
     private void spawnEnemies() {
@@ -137,7 +54,7 @@ public class LevelOne extends BaseLevel {
             previousSpawn = runTime;
         }
 
-        if (runTime > previousSpawn + interval && runTime < previousSpawn + interval + variation*4) {
+        if (runTime > previousSpawn + interval && runTime < previousSpawn + interval + variation * 4) {
             enemies.add(EnemyFactory.createWasp());
             previousSpawn = runTime;
         }
