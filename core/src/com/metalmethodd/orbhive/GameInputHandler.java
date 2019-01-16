cd2b7ac3f24880b01011d9bba4825932b8a1b6a0
metalMethod <igorbusquets@gmail.com>
2019-01-16 21:57:23 +0000

    first implementation of touch drag controls

--- core/src/com/metalmethodd/orbhive/GameInputHandler.java
+++ core/src/com/metalmethodd/orbhive/GameInputHandler.java
@@ -3,11 +3,17 @@ package com.metalmethodd.orbhive;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 import com.metalmethodd.orbhive.gameobjects.Player;
 
 public class GameInputHandler implements InputProcessor {
 
     private final Player player;
+    private Vector2 touchPos = new Vector2();
+    private Vector2 dragPos = new Vector2();
+    private float touchRadius = 200f;
+
 
     public GameInputHandler(Player player) {
 
@@ -86,9 +92,71 @@ public class GameInputHandler implements InputProcessor {
         return false;
     }
 
+
+    /**
+     * @Override public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+     * player.shoot();
+     * return true;
+     * }
+     * @Override public boolean touchDragged(int screenX, int screenY, int pointer) {
+     * return false;
+     * }
+     */
+
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+    public boolean touchDown(
+            int screenX,
+            int screenY,
+            int pointer,
+            int button) {
+        touchPos.set(screenX, Gdx.graphics.getHeight() - screenY);
+
         player.shoot();
+
+        return true;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        dragPos.set(screenX, Gdx.graphics.getHeight() - screenY);
+        float distance = touchPos.dst(dragPos);
+
+        /**
+         if (distance <= touchRadius){
+         //  * gives you a 'natural' angle
+         float angle =
+         MathUtils.atan2(
+         touchPos.x - dragPos.x, dragPos.y - touchPos.y)
+         * MathUtils.radiansToDegrees + 90;
+         if (angle < 0)
+         angle += 360;
+         // move according to distance and angle
+         } else
+         {
+         // keep moving at constant speed
+         }
+         */
+
+        if (dragPos.x > touchPos.x) {
+            player.moveForward();
+            System.out.println("drag forward");
+        }
+
+        if (dragPos.x < touchPos.x) {
+            player.moveBack();
+            System.out.println("drag back");
+        }
+
+        if (dragPos.y > touchPos.y) {
+            player.moveDown();
+            System.out.println("drag down");
+        }
+
+        if(dragPos.y < touchPos.y){
+            player.moveUp();
+            System.out.println("drag up");
+        }
+
         return true;
     }
 
@@ -97,10 +165,6 @@ public class GameInputHandler implements InputProcessor {
         return false;
     }
 
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
 
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
